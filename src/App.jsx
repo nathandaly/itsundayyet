b944f1ca2d8a9b0e6a7e9fda5ab950a4bd39b6a2
Nate Daly <nathan.daly@jump24.co.uk>
2022-01-20 16:01:17 +0000

    Adding initial files.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+export default function Example() {
+  return (
+    <>
+      <main
+        className="min-h-full bg-top bg-cover sm:bg-top"
+        style={{
+          backgroundImage:
+            'url("https://wallpapercave.com/wp/wp4195668.jpg")',
+        }}
+      >
+        <div className="px-4 py-16 mx-auto text-center max-w-7xl sm:px-6 sm:py-24 lg:px-8 lg:py-48">
+          <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-white sm:text-5xl">
+            Is it Sunday yet?
+          </h1>
+          <p className="mt-8 text-4xl font-extrabold tracking-tight text-black sm:text-5xl">
+            No :(
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
