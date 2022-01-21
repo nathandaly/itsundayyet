de7498ba2999a3a6e4e7fcd8309103a10b542fd0
Nate Daly <nathan.daly@jump24.co.uk>
2022-01-21 09:10:54 +0000

    Added test for Firdays.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import JSConfetti from 'js-confetti'
 import useIsItSunday from "./hooks/useIsItSunday";
+import useIsItFriday from "./hooks/useIsItFriday";
 
 export default function App() {
   const isItSunday = useIsItSunday();
+  const isItFriday = useIsItFriday();
   const canvas = document.getElementById('main');
   const jsConfetti = new JSConfetti({ canvas });
 
@@ -28,11 +30,14 @@ export default function App() {
         }}
       >
         <div className="px-4 py-16 mx-auto text-center max-w-7xl sm:px-6 sm:py-24 lg:px-8 lg:py-48">
-         { !isItSunday &&
+         { !isItSunday && !isItFriday &&
             <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-black sm:text-5xl">No 🙁</h1>
          }
-        { isItSunday &&
+        { isItSunday && !isItFriday &&
             <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-black sm:text-5xl">YES 😀</h1>
+         }
+        { isItFriday &&
+            <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-black sm:text-5xl">2 more sleeps 😴</h1>
          }
         </div>
       </main>
